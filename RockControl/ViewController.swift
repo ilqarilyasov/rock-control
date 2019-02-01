7558412dbef828f76dd1949d9d36eb2db48851d3
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-01 11:29:19 -0600

    Draw a lock screen

--- RockControl/ViewController.swift
+++ RockControl/ViewController.swift
@@ -10,38 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: - Super class lifecycle functions
+    
+    // MARK: - Properties
+    
+    var mainView: UIView!
+    var stackView: UIStackView!
+    var lockImageView: UIImageView!
+    var scrollView: LockControl!
+    var scrollingBallView: UIView!
+    
+    
+    // MARK: - Lifecycle functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         drawMainView()
+        drawStackView()
         drawLockImageView()
         drawScrollView()
         drawScrollingBallView()
     }
-    
-    // MARK: - Properties
-    
-    var mainView: UIView {
-        let view = UIView()
-        return view
-    }
-    
-    var lockImageView: UIImageView {
-        let imageView = UIImageView()
-        return imageView
-    }
-    
-    var scrollView: UIView {
-        let view = LockControl()
-        return view
-    }
-    
-    var scrollingBallView: UIView {
-        let view = UIView()
-        return view
-    }
+
     
     // MARK: - Bar button action
 
@@ -54,19 +44,69 @@ class ViewController: UIViewController {
     // MARK: - Drawings
     
     private func drawMainView() {
+        mainView = UIView()
+        view.addSubview(mainView)
         
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        mainView.widthAnchor.constraint(equalToConstant: 320).isActive = true
+        mainView.heightAnchor.constraint(equalToConstant: 320).isActive = true
+        
+        mainView.backgroundColor = .lightGray
+        mainView.layer.cornerRadius = 20
+    }
+    
+    private func drawStackView() {
+        stackView = UIStackView()
+        mainView.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerXAnchor.constraint(equalTo: mainView.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: mainView.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        stackView.layer.cornerRadius = 25
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 10
     }
     
     private func drawLockImageView() {
+        let lockImageView = UIImageView()
+        stackView.addArrangedSubview(lockImageView)
         
+        lockImageView.translatesAutoresizingMaskIntoConstraints = false
+        lockImageView.heightAnchor.constraint(equalToConstant: 220).isActive = true
+        
+        lockImageView.contentMode = .scaleAspectFit
+        lockImageView.image = UIImage(named: "lock")!
     }
     
     private func drawScrollView() {
+        scrollView = LockControl()
+        stackView.addArrangedSubview(scrollView)
         
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        scrollView.layer.cornerRadius = 25
+        scrollView.backgroundColor = .gray
     }
     
     private func drawScrollingBallView() {
+        scrollingBallView = UIView()
+        scrollView.addSubview(scrollingBallView)
         
+        scrollingBallView.translatesAutoresizingMaskIntoConstraints = false
+        scrollingBallView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10).isActive = true
+        scrollingBallView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor).isActive = true
+        scrollingBallView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        scrollingBallView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        scrollingBallView.layer.cornerRadius = 25
+        scrollingBallView.backgroundColor = .black
     }
 }
 
